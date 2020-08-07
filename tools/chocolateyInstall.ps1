319d1139be495f943a3d189dca3e9603a5929fe8
Michael Armbruster <michael@armbrust.me>
2020-08-07 11:05:39 +0200

    Update to Zstandard 1.4.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'zstandard'
-$packageVersion = '1.4.0'
-$urlArray = @('https://github.com/facebook/zstd/releases/download/v1.4.0/zstd-v1.4.0-win32.zip', 'https://github.com/facebook/zstd/releases/download/v1.4.0/zstd-v1.4.0-win64.zip')
+$packageVersion = '1.4.5'
+$urlArray = @('https://github.com/facebook/zstd/releases/download/v1.4.5/zstd-v1.4.5-win32.zip', 'https://github.com/facebook/zstd/releases/download/v1.4.5/zstd-v1.4.5-win64.zip')
 $url = $urlArray[0]
-$checksum = '123c0b659408065773f350842bf45dca318734eb635d0dc3ccced8005ea50062ef7560095aa7b285362013ea40324242aa0fb3e66c5f6c1e0f83d994015032b4'
+$checksum = '5c90dfda196e2fc5a130fb72d5a2586f50a3d48e57d15c7e3b07220a49c7c6320af1801782ea2be358c0a4911ee8e90e6ec6cd034b2a4631d3c8b058694bd563'
 $checksumType = 'sha512'
 $url64 = $urlArray[1]
-$checksum64 = '2643192f12ee1f7ed7663b85d74aa12756ad073608caa7f124e245183d46afb12059786a20c52949177c0c706ad8c02cdc0fa67e3f283f0d610faa6990f6b725'
+$checksum64 = '371623c5bbc07e8dfe8ffecb5fc056aba92e3dc74f2a7f82a9d438a5a14e3118080b27c030aea4c5da88822f9d8f007b6dc4706e0936916c96927e6dfa2c8787'
 $checksumType64 = 'sha512'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
