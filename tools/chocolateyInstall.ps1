7b926d1ffa4379f0f0e9ec8d92e77f07d7d43772
Michael Armbruster <6783670+maki-chan@users.noreply.github.com>
2021-03-19 21:51:36 +0100

    Fix wrong checksums
    Zstd changed the ZIP files for their releases which breaks the current install script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'zstandard'
-$packageVersion = '1.4.9'
+$packageVersion = '1.4.9.20210319'
 $urlArray = @('https://github.com/facebook/zstd/releases/download/v1.4.9/zstd-v1.4.9-win32.zip', 'https://github.com/facebook/zstd/releases/download/v1.4.9/zstd-v1.4.9-win64.zip')
 $url = $urlArray[0]
-$checksum = 'dc34b933f94c6a9ac0a71df033bacd58c6746cd781b84557acc81805e9d667aebe41dd68c11cc6d0a8a2f72b41ee889f33050b03bd5445397fddbe1140c77483'
+$checksum = '499edf019e55abe68e558900972012974944018ad39faec826de86e96288b670cf980e7b4eaecb8df9734992d6c074211bfcbf782d9f5d09b4d1bf064489a797'
 $checksumType = 'sha512'
 $url64 = $urlArray[1]
-$checksum64 = '4a573f326c0a2bb9b6c8fb748603265569df2e96c2f771c043fb09e33611be6b2041ba362d889756cfeb4ccee994a004502d283848fd61d427e82022492c8e19'
+$checksum64 = '65ed3a6427dc267d912e55a0faf18686edff90d6950eb9114002ef090935bb712bf92a6dc3285b06f7d22c33f012b6ea2f2411ff5ebc7ad4dbee4b84db6016bf'
 $checksumType64 = 'sha512'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
